831e60b78d3f4b889c714512b9b7a9472a890289
softm-rauljuarez <raul.juarez.ce@softm.com.mx>
2023-02-25 03:51:09 -0600

    UI: SplashScreen

---
+++ app/src/main/java/mx/com/pokemon/project/views/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package mx.com.pokemon.project.views.splashscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import mx.com.pokemon.project.databinding.SplashscreenActivityBinding
+import mx.com.pokemon.project.interfaces.ISplashScreen
+
+class SplashScreenActivity: AppCompatActivity(), ISplashScreen {
+    private lateinit var binding: SplashscreenActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setInitView()
+        initSplashScreen()
+    }
+
+    private fun setInitView(){
+        binding = SplashscreenActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
